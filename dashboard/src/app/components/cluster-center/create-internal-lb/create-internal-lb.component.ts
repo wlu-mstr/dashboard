ad909e83e79b0f2cd5f69962fa514d87412940f8
liangzhifeng <zhifeng.liang@honeywell.com>
2017-09-30 17:37:32 +0800

    新页面

---
+++ dashboard/src/app/components/cluster-center/create-internal-lb/create-internal-lb.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit,Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-create-internal-lb',
+  templateUrl: './create-internal-lb.component.html',
+  styleUrls: ['./create-internal-lb.component.scss']
+})
+export class CreateInternalLBComponent implements OnInit {
+
+  objs: any = [];
+  alertMessage: string;
+  alertType: string;
+  //查询条件
+  startTime:Date;
+  endTime:Date;
+  manualFlag:string = '';
+  incomeNumber:string = '';
+  serviceType:string = '';
+  totalItems: number = 1;
+  currentPage: number = 1;
+  //判定alert输入值变化
+  alertNumber = 1;
+  //蒙层显隐
+  showModal = false;
+
+  query: any;
+  zh:any;
+  constructor(@Inject('help') private helpService) { }
+
+  ngOnInit() {
+
+  }
+
+
+  //获取列表
+  getSellerList() {
+
+  }
+
+
+
+}
